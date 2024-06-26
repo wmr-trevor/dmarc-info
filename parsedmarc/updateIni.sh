2ce0e70cfc76f2e8bf25cdd4dd9b229943ca7cf0
Trevor Reimer <trevor@wallsmetalroofing.com>
2024-06-26 13:11:40 -0300

    setup open search

--- parsedmarc/updateIni.sh
+++ parsedmarc/updateIni.sh
@@ -2,3 +2,5 @@ source .env
 
 sed -i "s|GOOGLE_ACCOUNT|${GOOGLE_ACCOUNT}|g" /parsedmarc.ini
 sed -i "s|GOOGLE_PASSWORD|${GOOGLE_PASSWORD}|g" /parsedmarc.ini
+
+sed -i "s|OPENSEARCH_INITIAL_ADMIN_PASSWORD|${OPENSEARCH_INITIAL_ADMIN_PASSWORD}|g" /parsedmarc.ini
